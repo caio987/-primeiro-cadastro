280dc23ee8027b30328cddc58c48aa1dd89715c8
caio987 <160530202+caio987@users.noreply.github.com>
2025-02-26 10:41:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// var nome = document.getElementById("nome").value
+var nome = prompt("Digite seu nome de úsuario:")
+var cpf = prompt("Digite seu CPF de úsuario:")
+var tel = prompt("Digite seu telefone de úsuario:")
+var email = prompt("Digite seu e-mail de úsuario:")
+
+document.write("Úsuario: " + nome)
+document.write("<br>")
+document.write("CPF: " + cpf)
+document.write("<br>")
+document.write("Telefone: " + tel)
+document.write("<br>")
+document.write("E-mail: " + email)
